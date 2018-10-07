38ba77fcd83271d76fbb34e1fd2c7fc7a0064dde
Susmit <susmit.py@gmail.com>
2018-10-07 13:31:49 +0530

    aa

---
+++ app/src/main/java/susmit/sucideprevention/SomeoneNeedsHelp.java
@@ -0,0 +1,15 @@
+package susmit.sucideprevention;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SomeoneNeedsHelp extends AppCompatActivity {
+    Button willCall, callMe;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_someone_needs_help);
+
+    }
+}
